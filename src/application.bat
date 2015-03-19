e49a85724b58e668c76dbb8f198759a7c89f9725
Charles <freyaseiseko@gmail.com>
2015-03-19 16:24:57 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,2 +1,6 @@
-echo "version 1"
-echo "Build : 10:00"
+### application.bat - 2015 - Charles-Erwan DOS SANTOS ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
